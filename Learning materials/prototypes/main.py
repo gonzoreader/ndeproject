a75ddeb5b27316508d03fdbaafe64d7a39d83957
gonzoreader <gonzoreader@gmail.com>
2024-01-07 23:27:55 -0600

    leaving off around keystrokes

--- Learning materials/prototypes/main.py
+++ Learning materials/prototypes/main.py
@@ -26,19 +26,20 @@ def linesui():
     pygame.draw.line(screen, (black), (0,850), (1920, 850), 6)
     pygame.draw.line(screen, (black), (400,850), (400, 1080), 6)
     pygame.draw.line(screen, (black), (1000, 850), (1000, 1080), 6)
-    for x in range(0, 850, 85):
-        pygame.draw.line(screen, (black), (0,x), (1920,x), 5)
-    for x in range(0, 1920, 120):
-        pygame.draw.line(screen, (black), (x,0), (x,850), 5)
+#    for x in range(0, 850, 85):
+#        pygame.draw.line(screen, (black), (0,x), (1920,x), 5)
+#    for x in range(0, 1920, 120):
+#        pygame.draw.line(screen, (black), (x,0), (x,850), 5)
 
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 10
 playerY = 2
 
-# Defining a player function so that i can call this code in my while loop
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+# Defining a player function so that i can call this code in my while loop.
+def player(x,y):
+    screen.blit(playerImg, (x, y))
 
 # Setting up the while loop with an always true variable
 running = True
@@ -47,16 +48,23 @@ running = True
 while running:
     # Changing the color of the window
     screen.fill((110, 110, 110))
+
     # Event for loop. Pygame uses events for mouse movement, key presses etc.
     # All events get passed through this loop so that they are executed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+        # if keystroke is pressed check whether it is right or left
+        #if event.type == pygame.KEYDOWN:
+            #if event.key == pygame.K_LEFT:                             ##########################################<---------------------- where i left off
+
+
+    
     # Calling the linesui function
     linesui()
     # Calling the player function
-    player()
+    player(playerX,playerY)
     # Updating the display every tick
     pygame.display.update()
 
