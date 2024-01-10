b119dd4532a3340c79f01204e80263b5c787115d
gonzoreader <gonzoreader@gmail.com>
2024-01-10 15:58:29 -0600

    finishing up collision ad levels

--- Learning materials/prototypes/chttest.py
+++ Learning materials/prototypes/chttest.py
@@ -4,9 +4,12 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1920, 1080))
 black = (0, 0, 0)
 grey = (80, 80, 80)
+
+font = pygame.font.Font('Grand9K Pixel.ttf', 36)
+
+screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("NDE Project")
 icon = pygame.image.load('skeleton.png')
 pygame.display.set_icon(icon)
@@ -20,6 +23,9 @@ playerY_change = 0
 
 ch = pygame.image.load('crosshair.png')
 
+def display_text(text, color, x, y):
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, (x, y))
 
 def crosshair():
     screen.blit(ch, (mpos, mpos))
@@ -41,10 +47,22 @@ def map2T():
     pygame.draw.rect(screen, grey, pygame.Rect(0, 680, 1920, 400))
 
 
+def map3():
+    pygame.draw.rect(screen, (grey), pygame.Rect(0, 0, 200, 400))
+    pygame.draw.rect(screen, (grey), pygame.Rect(0, 680, 200, 400))
+    pygame.draw.rect(screen, grey, pygame.Rect(0, 900, 1920, 400))
+
+
+def map4():
+    pygame.draw.rect(screen, (grey), pygame.Rect(0, 0, 1920, 200))
+
+
 current_level = 0
 maps_data = [
     map1,
     map2T,
+    map3,
+    map4,
     # Add more maps as needed
 ]
 
@@ -73,17 +91,35 @@ while running:
                 playerX_change = 0
             if event.key == pygame.K_w or event.key == pygame.K_s:
                 playerY_change = 0
-    #if playerX >= 1839:
 
 
 
-
+    if current_level == 0:
+        # Displaying text on map1
+        display_text("WASD to move", (255, 255, 255), 800, 540)
+    if current_level == 2:
+        display_text('Theres gonna be some kind of monster here in the future', (grey), 800, 600)
 
     playerY += playerY_change
     playerX += playerX_change
+    # third map collision
+    if current_level == 2:
+        if playerX <= 195 and playerY >= 630:
+            playerY = 630
+        if playerX <= 195 and playerY <= 400:
+            playerY = 400
+        if playerY <= 390 and playerX <= 201:
+            playerX = 201
+        if playerY >= 680 and playerX <= 201:
+            playerX = 201
+        if playerX <= 10:
+            current_level = 1
+            playerX = 1890
+        if playerY >= 850 and playerX >= 201:
+            playerY = 850
     # second map collision
     if current_level == 1:
-        if playerX >= 1839:
+        if playerX >= 1900:
             playerX = 30
             current_level = 2
         elif playerY >= 630:
@@ -99,6 +135,9 @@ while running:
         if playerX <= 5:
             current_level = 0
             playerX = 1899
+        if playerY <= 10:
+            current_level = 3
+            playerY = 1060
     # first map collision
     if current_level == 0:
         if playerX <= 51:
